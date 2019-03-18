dfa96de90eada42bff3a68b86540b44cd0a296f2
Lindsey Catlett <lindsey.catlett@gmail.com>
2019-03-17 21:04:44 -0400

    Update release script.

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -30,7 +30,7 @@ rm -fr vendor
 composer install --no-dev
 
 #
-# Tag & build master branch
+# Tag & build acquia branch
 #
 box build
 
@@ -42,7 +42,7 @@ git checkout gh-pages
 cp cachetool.phar downloads/cachetool-${TAG}.phar
 git add downloads/cachetool-${TAG}.phar
 
-if [ "$BRANCH" == "master" ]; then
+if [ "$BRANCH" == "acquia" ]; then
   cp cachetool.phar downloads/cachetool.phar
   git add downloads/cachetool.phar
 fi
@@ -55,13 +55,13 @@ fi
 
 JSON='name:"cachetool.phar"'
 JSON="${JSON},sha1:\"${SHA1}\""
-JSON="${JSON},url:\"http://gordalina.github.io/cachetool/downloads/cachetool-${TAG}.phar\""
+JSON="${JSON},url:\"http://lcatlett.github.io/cachetool/downloads/cachetool-${TAG}.phar\""
 JSON="${JSON},version:\"${TAG}\""
 
 if [ -f cachetool.phar.pubkey ]; then
     cp cachetool.phar.pubkey pubkeys/cachetool-${TAG}.phar.pubkeys
     git add pubkeys/cachetool-${TAG}.phar.pubkeys
-    JSON="${JSON},publicKey:\"http://gordalina.github.io/cachetool/pubkeys/cachetool-${TAG}.phar.pubkey\""
+    JSON="${JSON},publicKey:\"http://lcatlett.github.io/cachetool/pubkeys/cachetool-${TAG}.phar.pubkey\""
 fi
 
 #
@@ -74,9 +74,9 @@ git add manifest.json
 git commit -m "Bump version ${TAG}"
 
 #
-# Go back to master
+# Go back to acquia
 #
-git checkout master
+git checkout acquia
 
 git tag ${TAG}
 git push origin gh-pages
